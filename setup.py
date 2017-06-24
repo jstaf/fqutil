eef91aa258e71b49292000873c9ac52e05d5e11e
Jeff Stafford <jeff.stafford@live.com>
2017-06-24 07:35:06 -0400

    package-ize toolkit

---
+++ setup.py
@@ -0,0 +1,19 @@
+import glob
+from setuptools import setup
+
+setup(
+        name='fqutils',
+        version='1.0',
+        description='Several useful tools for manipulating FASTQ files.',
+        url='https://github.com/jstaf/fqutils',
+        author='Jeff Stafford',
+        license='MIT',
+        packages=['fqutils'],
+        install_requires=[
+            'biopython',
+            'numpy'
+        ]
+        scripts=glob('bin/*'),
+        zip_safe=False
+        )
+

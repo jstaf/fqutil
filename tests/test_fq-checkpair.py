3e6e46f0b8d50953f7cf8437210e810b17a4bf6a
Jeff Stafford <jeff.stafford@live.com>
2017-06-25 11:31:37 -0400

    add better tests for checkpair

---
+++ tests/test_fq-checkpair.py
@@ -0,0 +1,18 @@
+"""Check that fq-checkpair is properly detecting mismatched reads"""
+
+import os
+
+def test_are_mismatched():
+    assert os.system('./fq-checkpair tests/r1.fastq tests/r2.fastq') == 256
+
+
+def test_are_mismatched_gz():
+    assert os.system('./fq-checkpair tests/r1.fastq.gz tests/r2.fastq.gz') == 256
+
+
+def test_are_matched():
+    assert os.system('./fq-checkpair tests/r1_cmn.fastq tests/r2_cmn.fastq') == 0
+
+
+def test_are_matched_gz():
+    assert os.system('./fq-checkpair tests/r1_cmn.fastq.gz tests/r2_cmn.fastq.gz') == 0
--- tests/test_util.py
+++ tests/test_util.py
@@ -5,6 +5,7 @@ def test_prefix_extension():
     assert util.prefix_extension('r1.fastq.gz', '_common') == 'r1_common.fastq.gz'
     assert util.prefix_extension('tests/r1.fastq.gz', '_common') == 'tests/r1_common.fastq.gz'
 
+
 def test_is_gzip():
     assert not util.is_gzip('tests/r1.fastq')
     assert util.is_gzip('tests/r1.fastq.gz')

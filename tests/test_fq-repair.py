170e2b62870a4f7e900734fbddd21807509b02d3
Jeff Stafford <jeff.stafford@live.com>
2017-06-25 11:41:58 -0400

    add tests for repair

---
+++ tests/test_fq-repair.py
@@ -0,0 +1,14 @@
+"""Check that reads are being properly repaired"""
+
+import os
+
+def test_repair():
+    os.system('./fq-repair -u tests/r1.fastq tests/r2.fastq')
+    assert os.system('./fq-checkpair tests/r1_common.fastq tests/r2_common.fastq') == 0
+    assert os.system('./fq-checkpair tests/r1_unique.fastq tests/r2_unique.fastq') == 256
+
+
+def test_repair_gz():
+    os.system('./fq-repair -u tests/r1.fastq.gz tests/r2.fastq.gz')
+    assert os.system('./fq-checkpair tests/r1_common.fastq.gz tests/r2_common.fastq.gz') == 0
+    assert os.system('./fq-checkpair tests/r1_unique.fastq.gz tests/r2_unique.fastq.gz') == 256

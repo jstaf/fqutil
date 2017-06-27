bacc6bb7167f3e29385bbf8ec7be9d67bb74491d
Jeff Stafford <jeff.stafford@live.com>
2017-06-27 16:01:40 -0400

    improve test slightly

--- tests/test_fq_repair.py
+++ tests/test_fq_repair.py
@@ -11,7 +11,7 @@ def delete_test_files(extension):
 
 def test_repair():
     delete_test_files('.fastq')
-    os.system('./fq-repair -u tests/r1.fastq tests/r2.fastq')
+    assert os.system('./fq-repair -u tests/r1.fastq tests/r2.fastq') == 0
     assert os.path.isfile('tests/r1_common.fastq')
     assert os.path.isfile('tests/r2_common.fastq')
     assert os.system('./fq-checkpair tests/r1_common.fastq tests/r2_common.fastq') == 0
@@ -22,7 +22,7 @@ def test_repair():
 
 def test_repair_gz():
     delete_test_files('.fastq.gz')
-    os.system('./fq-repair -u tests/r1.fastq.gz tests/r2.fastq.gz')
+    assert os.system('./fq-repair -u tests/r1.fastq.gz tests/r2.fastq.gz') == 0
     assert os.path.isfile('tests/r1_common.fastq.gz')
     assert os.path.isfile('tests/r2_common.fastq.gz')
     assert os.system('./fq-checkpair tests/r1_common.fastq.gz tests/r2_common.fastq.gz') == 0
